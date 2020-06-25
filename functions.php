00816ca7984b60e8e54f2112d5b0f9abb02af146
Mark Heydon <markheydon@mhcg.co.uk>
2020-06-25 13:18:09 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Standard Avada custom styles override.
+ */
+function theme_enqueue_styles() {
+    $parent_style = 'avada-stylesheet';
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+}
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+/**
+ * Standard Avada language setup for child theme.
+ */
+function avada_lang_setup() {
+        $lang = get_stylesheet_directory() . '/languages';
+        load_child_theme_textdomain( 'Avada', $lang );
+}
+add_action( 'after_setup_theme', 'avada_lang_setup' );
