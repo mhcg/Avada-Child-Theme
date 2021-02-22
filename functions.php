3da92ab75621eea2b25d397d30527cde012e04b3
Mark Heydon <markheydon@mhcg.co.uk>
2021-02-22 19:05:00 +0000

    Updated for Avada v7.2 child theme format.

--- functions.php
+++ functions.php
@@ -4,14 +4,14 @@
  * Standard Avada custom styles override.
  */
 function theme_enqueue_styles() {
-    $parent_style = 'avada-stylesheet';
-    wp_enqueue_style( 'child-style',
+    wp_enqueue_style(
+        'child-style',
         get_stylesheet_directory_uri() . '/style.css',
-        array( $parent_style ),
+        [],
         wp_get_theme()->get('Version')
     );
 }
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', 20 );
 
 /**
  * Standard Avada language setup for child theme.
